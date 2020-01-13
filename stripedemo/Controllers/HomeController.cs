39850f6038cb5177e7802683229d02739f0686e0
mac <monika.yadav@overseasits.com>
2020-01-13 14:26:07 +0530

    Initial check-in of module stripedemo

---
+++ stripedemo/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using Stripe;
+using stripedemo.Models;
+
+namespace stripedemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        string APIKey= "sk_test_QH3gMc3gz7gErIluQpaeggK700R0BXpPkP";
+        public ActionResult Index()
+        {
+            
+           
+            dynamic data=Listplan();
+           
+            return View();
+        }
+        public dynamic Create(Common model)
+        {
+            StripeConfiguration.ApiKey = APIKey;
+
+            var options = new CustomerCreateOptions
+            {
+                Name=model.name,
+                Email=model.email,
+                Description = model.description,
+            };
+            var service = new CustomerService();
+           return service.Create(options);
+        }
+        public dynamic Pay()
+
+        {
+            StripeConfiguration.ApiKey = APIKey;
+
+            var service = new PaymentIntentService();
+            var options = new PaymentIntentCreateOptions
+            {
+                PaymentMethodTypes = new List<string> { "card" },
+                Amount = 1099,
+                Currency = "inr",
+                Customer = "cus_GXQijo0EPiMTkd"
+            };
+           return service.Create(options);
+        }
+        public dynamic Sheduldeplan(Common model)
+        {
+            StripeConfiguration.ApiKey = APIKey;
+            dynamic d= Create(model);
+            model.cusid = "cus_GXQijo0EPiMTkd";
+            var options = new SubscriptionCreateOptions
+            {
+                Customer = model.cusid,
+                Items = new List<SubscriptionItemOptions>
+                {
+                 new SubscriptionItemOptions
+                 {
+                      Plan = model.palnid,
+                 },
+                },
+            };
+
+            var service = new SubscriptionService();
+          return  service.Create(options);
+        }
+        public dynamic Listplan()
+        {
+            StripeConfiguration.ApiKey = APIKey;
+
+            var options = new PlanListOptions { };
+            var service = new PlanService();
+           return service.List(options);
+        }
+
+      
+    }
+}
---
+++ stripedemo/Models/Common.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace stripedemo.Models
+{
+    public class Common
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string description { get; set; }
+        public string palnid { get; set; }
+        public string cusid { get; set; }
+    }
+    public class price
+    {
+        public long pricedata { get; set; }
+    }
+}
